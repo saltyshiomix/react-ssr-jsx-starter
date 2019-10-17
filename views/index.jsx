c2444b6164f3d35f77dbd1eb2b4349127dbb8af6
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-17 18:24:06 +0900

    Upgrade @react-ssr/express@^0.4.1

--- views/index.jsx
+++ views/index.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import { Layout } from '../components';
 
-const Index = ({ user }) => {
+const Index = (props) => {
+  const {
+    user,
+    script,
+  } = props;
+
   const [message, setMessage] = useState('waiting...');
 
   const onClick = () => setMessage('This is a react-ssr!');
@@ -9,6 +14,7 @@ const Index = ({ user }) => {
   return (
     <Layout
       title="An example of @react-ssr/express"
+      script={script}
     >
       <p>Hello {user.name}!</p>
       <button onClick={onClick}>Click Me</button>
